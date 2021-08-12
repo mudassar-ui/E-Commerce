d03754d20476d24f9fac0ef37e2f494481253c9f
mudassar-ui <mudassark15@yahoo.com>
2021-08-12 21:39:14 +0530

    Add cart model and filters

--- lib/model/cart.dart
+++ lib/model/cart.dart
@@ -1,1 +1,19 @@
+class CartItem {
+  final String id;
+  final String title;
+  final int quantity;
+  final double price;
 
+  CartItem({
+    required this.id,
+    required this.title,
+    required this.quantity,
+    required this.price,
+  });
+
+  //To print string of CartItem Object on Consule
+  @override
+  String toString() {
+    return 'CartItem: {id: $id, title: $title,quantity: $quantity, price: $price}';
+  }
+}
