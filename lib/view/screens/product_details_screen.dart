a88663936c72c52868266d4ef0cbe927383e1bd3
mudassar-ui <mudassark15@yahoo.com>
2021-08-13 21:33:35 +0530

    Product details page added

--- lib/view/screens/product_details_screen.dart
+++ lib/view/screens/product_details_screen.dart
@@ -4,15 +4,50 @@ import '../../controller/products_controller.dart';
 
 class ProductDetailsScreen extends StatelessWidget {
   final ProductsController productsController = Get.put(ProductsController());
-  final id = Get.arguments;
 
   @override
   Widget build(BuildContext context) {
-    final loadedProduct = productsController.findById(id);
-    print(loadedProduct);
+    final productId = Get.arguments;
+
+    final loadedProduct = productsController.findById(productId);
     return Scaffold(
       appBar: AppBar(
-        title: Text('${loadedProduct.title}'),
+        title: Text(loadedProduct.title),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 300,
+              width: double.infinity,
+              child: Image.network(
+                loadedProduct.imageUrl,
+                fit: BoxFit.cover,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              '\$${loadedProduct.price}',
+              style: TextStyle(
+                color: Colors.grey,
+                fontSize: 20,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+                width: double.infinity,
+                padding: EdgeInsets.symmetric(horizontal: 10),
+                child: Text(
+                  loadedProduct.description,
+                  textAlign: TextAlign.center,
+                  softWrap: true,
+                ))
+          ],
+        ),
       ),
     );
   }
